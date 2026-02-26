9db50cdada91efacce90679512755e9a788c3a94
joshuabituin59-hash <joshuabituin59@gmail.com>
2026-02-26 10:13:55 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,8 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("../controllers/userControllers");
+const express=require('express');
+const router=express.Router();
+const userController=require('../controllers/userController');
+
+
 //Route to get all users
-router.get("/users", userController.getAllUsers);
-//route to search a user by id
-// router.get("/users/:id", userController.getUserById);
-router.get("/users/:last_name", userController.getUserByLastName);
-//create
-router.post("/users", userController.createUser);
-//route to edit a user
-router.put("/users", userController.updateUser);
-//route to delete a user
-router.delete("/users", userController.deleteUser);
-module.exports = router;
+router.get('/users',userController.getAllUsers);
+module.exports=router;
